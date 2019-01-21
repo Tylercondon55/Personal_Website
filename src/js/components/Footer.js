b6bf2fcc8b75aef27cca045ecd6e22304e8176d0
Tyler Condon <tcondon@us.ibm.com>
2019-01-21 02:35:17 -0800

    coverted website to react

---
+++ src/js/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div class = "footer">
+        <p>Tyler Condon &bull;
+        <a href="https://www.linkedin.com/in/tyler-condon-368393a3/" rel="noopener noreferrer" target="_blank">Linkedin</a> &bull;
+        <a href="https://github.com/Tylercondon55/" rel="noopener noreferrer" target="_blank">GitHub</a> &bull;
+        <a href="img/Resume.pdf" rel="noopener noreferrer" target="_blank">Resume</a> &bull;
+        <a href="mailto:Tylercondon55@gmail.com">tylercondon55@gmail.com</a>
+        </p>
+      </div>
+    );
+  }
+}
